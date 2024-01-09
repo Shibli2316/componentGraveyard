9e15c14e38c977d9756077c82b098a39e180ee1e
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2024-01-09 20:06:54 +0530

    Testimonials page

---
+++ src/pages/TestimonialPage.js
@@ -0,0 +1,11 @@
+import Testimonial from "../features/testimonial/Testimonial";
+
+function TestimonialPage() {
+    return (
+        <>
+            <Testimonial />
+        </>
+    );
+}
+
+export default TestimonialPage;
