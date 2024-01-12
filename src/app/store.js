58e009b3656e3d57cbaede1dda5c8993493fd74d
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2024-01-13 00:08:05 +0530

    API setting

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import acrylicReducer from '../features/product/acrylicSlice';
+import doodleReducer from '../features/product/doodleSlice';
+import lineartReducer from '../features/product/lineartSlice';
+import muralReducer from '../features/product/muralSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    acrylic: acrylicReducer,
+    doodle: doodleReducer,
+    lineart: lineartReducer,
+    mural: muralReducer,
   },
 });
